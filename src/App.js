a20a26738c03a1d7a80cd0488055f14462a62b88
Aaditya Doiphode <aadityad27@gmail.com>
2022-09-17 22:35:48 -0400

    api calls work

--- src/App.js
+++ src/App.js
@@ -1,97 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Home.js";
-import Ticker from "react-ticker";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      {/* <h1>Welcome to the Stock Site!</h1>
-      <h5>
-        Feel free to browse the top and worst performing stocks of the day, or
-        enter a stock below to see that stock's specific performance and metrics
-      </h5>
-      <table style="border:1px solid black;margin-left:auto;margin-right:auto;">
-        <tr>
-          <th>Top Performers</th>
-        </tr>
-        <tr>
-          <td>Symbol</td>
-          <td>% Gained</td>
-        </tr>
-
-        <tr>
-          <td>1. $APPL</td>
-          <td>25.06%</td>
-        </tr>
-        <tr>
-          <td>2. $GOOG</td>
-        </tr>
-        <tr>
-          <td>3. $AMZN</td>
-        </tr>
-        <tr>
-          <td>4. $TSLA</td>
-        </tr>
-        <tr>
-          <td>5. $GVDN</td>
-        </tr>
-        <tr>
-          <td>6. $EXXN</td>
-        </tr>
-        <tr>
-          <td>7. $CHEV</td>
-        </tr>
-        <tr>
-          <td>8. $MMM</td>
-        </tr>
-        <tr>
-          <td>9. $MDMA</td>
-        </tr>
-        <tr>
-          <td>10. $GM</td>
-        </tr>
-      </table>
-      <table style="border:1px solid black;margin-left:auto;margin-right:auto;">
-        <tr>
-          <th>Top Losers</th>
-        </tr>
-        <tr>
-          <td>Symbol</td>
-          <td>% Lost</td>
-        </tr>
-        <tr>
-          <td>1. $APPL</td>
-        </tr>
-        <tr>
-          <td>2. $GOOG</td>
-        </tr>
-        <tr>
-          <td>3. $AMZN</td>
-        </tr>
-        <tr>
-          <td>4. $TSLA</td>
-        </tr>
-        <tr>
-          <td>5. $GVDN</td>
-        </tr>
-        <tr>
-          <td>6. $EXXN</td>
-        </tr>
-        <tr>
-          <td>7. $CHEV</td>
-        </tr>
-        <tr>
-          <td>8. $MMM</td>
-        </tr>
-        <tr>
-          <td>9. $MDMA</td>
-        </tr>
-        <tr>
-          <td>10. $GM</td>
-        </tr>
-      </table> */}
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,70 @@
 import Ticker from "react-ticker";
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Axios from "axios";
 import "./Home.css";
 
 function Home() {
-  const getLosers = () => {
+  const _APIKEY = "54ff816144462eb7ce61a1fd81afb014";
+
+  const [topStocks, setTopStocks] = useState([]);
+  const [setLoserStocks, loserStocks] = useState();
+
+  const [name, setName] = useState("");
+
+  const handleInput = (event) => {
+    setName(event.target.value);
+  };
+
+  const logValue = () => {
     Axios.get(
-      "https://financialmodelingprep.com/api/v3/stock_market/losers?apikey=b95af4b41fdcc83df2c15801bdfdb363"
+      "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" +
+        name +
+        "&interval=60min&outputsize=full&apikey=ZGE19H5HOBZKLLHD"
     ).then((response) => {
       console.log(response);
     });
   };
+
+  const getGainers = () => {
+    Axios.get(
+      "https://financialmodelingprep.com/api/v3/stock_market/gainers?apikey=" +
+        _APIKEY
+    ).then((response) => {
+      for (var j = 0; j < 10; j++) {
+        var name = response.data[j].name;
+        var symbol = response.data[j].symbol;
+        var percent = response.data[j].changesPercentage;
+        var change = response.data[j].change;
+        var price = response.data[j].price;
+        var onestock = { name, symbol, percent, change, price };
+        setTopStocks([...topStocks, onestock]);
+      }
+      console.log(topStocks);
+    });
+  };
+
+  const getLosers = () => {
+    Axios.get(
+      "https://financialmodelingprep.com/api/v3/stock_market/losers?apikey=" +
+        _APIKEY
+    ).then((response) => {
+      console.log(response);
+      for (var j = 0; j < 10; j++) {
+        var name = response.data[j].name;
+        var symbol = response.data[j].symbol;
+        var percent = response.data[j].changesPercentage;
+        var change = response.data[j].change;
+        var price = response.data[j].price;
+        setLoserStocks([{ name, symbol, percent, change, price }]);
+      }
+    });
+  };
+
   return (
     <div>
       <h1>The Sheriff of Nottingham</h1>
       <h5>Stock data on your fingertips!</h5>
+      <button onClick={getGainers}>Get Gainers</button>
       <button onClick={getLosers}>Get Losers</button>
       {/* <div className="ticker-wrapper">
         <div className="bigheading">Breaking</div>
@@ -25,11 +75,50 @@ function Home() {
           </p>
         </div>
       </div> */}
+      <div>
+        <input onChange={handleInput} placeholder="Enter Stock Ticker" />
+        <button onClick={logValue}>Search</button>
+      </div>
       <div className="container">
-        <div className="square"></div>
-        <div className="graphGainer"></div>
-        <div className="square2"></div>
-        <div className="graphLoser"></div>
+        <div>
+          <h3>Top Gainers</h3>
+          <div className="containGain">
+            <div className="square">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Symbol</th>
+                    <th>Name</th>
+                    <th>Market Value</th>
+                    <th>Change</th>
+                    <th>Percent Change (%)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {topStocks.map((gainers, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{gainers.symbol}</td>
+                        <td>{gainers.name}</td>
+                        <td>{gainers.price}</td>
+                        <td>{gainers.change}</td>
+                        <td>{gainers.percent}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+            <div className="graphGainer"></div>
+          </div>
+        </div>
+        <div>
+          <h3>Top Losers</h3>
+          <div className="containLoss">
+            <div className="square2"></div>
+            <div className="graphLoser"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
